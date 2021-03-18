71d0e15545ad34a73ded22b38252eaa16d5f694b
Raul Mogos <raulmogos109@yahoo.com>
2021-03-18 18:55:06 +0200

    added more test cases for addStudent feature (2)

--- src/test/java/ssvv/example/service/ServiceTest.java
+++ src/test/java/ssvv/example/service/ServiceTest.java
@@ -25,6 +25,8 @@ public class ServiceTest {
     public void tearDown() throws Exception {
     }
 
+
+    // group
     @Test
     public void addStudentValidGroup() {
         Student student = new Student("2", "Raul Mogos", 935, "raul@raul.com");
@@ -32,6 +34,36 @@ public class ServiceTest {
         assertTrue(true);
         studentFileRepository.save(student);
         assertTrue(true);
+    }
+
+    @Test
+    public void addStudentValidGroupBVA0() {
+        Student student = new Student("2", "Raul Mogos", 0, "raul@raul.com");
+        studentValidator.validate(student);
+        assertTrue(true);
+        studentFileRepository.save(student);
+        assertTrue(true);
+    }
+
+    @Test
+    public void addStudentValidGroupBVA1() {
+        Student student = new Student("2", "Raul Mogos", 1, "raul@raul.com");
+        studentValidator.validate(student);
+        assertTrue(true);
+        studentFileRepository.save(student);
+        assertTrue(true);
+    }
+
+    @Test
+    public void addStudentInvalidGroupBVAMinus1() {
+        Student student = new Student("2", "Raul Mogos", -1, "raul@raul.com");
+        try {
+            studentValidator.validate(student);
+            assertTrue(false);
+            studentFileRepository.save(student);
+        } catch (ValidationException e) {
+            assertTrue(true);
+        }
     }
 
     @Test
@@ -46,9 +78,19 @@ public class ServiceTest {
         }
     }
 
+    // id
     @Test
     public void addStudentValidId() {
         Student student = new Student("1", "Raul Mogos", 935, "raul@raul.com");
+        studentValidator.validate(student);
+        assertTrue(true);
+        studentFileRepository.save(student);
+        assertTrue(true);
+    }
+
+    @Test
+    public void addStudentValidIdBVA() {
+        Student student = new Student("0", "Raul Mogos", 935, "raul@raul.com");
         studentValidator.validate(student);
         assertTrue(true);
         studentFileRepository.save(student);
@@ -83,7 +125,7 @@ public class ServiceTest {
 
     @Test
     public void addStudentInvalidIdNegative() {
-        Student student = new Student("-4", "Raul Mogos", 935, "raul@raul.com");
+        Student student = new Student("-1", "Raul Mogos", 935, "raul@raul.com");
         try {
             studentValidator.validate(student);
             assertTrue(false);
@@ -94,9 +136,19 @@ public class ServiceTest {
         }
     }
 
+    // name
     @Test
     public void addStudentValidName() {
         Student student = new Student("3", "Raul Mogos", 935, "raul@raul.com");
+        studentValidator.validate(student);
+        assertTrue(true);
+        studentFileRepository.save(student);
+        assertTrue(true);
+    }
+
+    @Test
+    public void addStudentValidNameBVA() {
+        Student student = new Student("3", "a", 935, "raul@raul.com");
         studentValidator.validate(student);
         assertTrue(true);
         studentFileRepository.save(student);
@@ -115,6 +167,7 @@ public class ServiceTest {
         }
     }
 
+    // email
     @Test
     public void addStudentValidEmail() {
         Student student = new Student("4", "Raul Mogos", 935, "raul@raul.com");
@@ -125,7 +178,30 @@ public class ServiceTest {
     }
 
     @Test
+    public void addStudentValidEmailBVA() {
+        Student student = new Student("1", "Raul Mogos", 935, "a");
+        studentValidator.validate(student);
+        assertTrue(true);
+        studentFileRepository.save(student);
+        assertTrue(true);
+    }
+
+    @Test
     public void addStudentInvalidEmail() {
+        Student student = new Student("4", "Raul Mogos", 935, "");
+        try {
+            studentValidator.validate(student);
+            assertTrue(false);
+            studentFileRepository.save(student);
+        } catch (ValidationException e) {
+            assertTrue(true);
+        }
+    }
+    
+    
+    // BVA
+    @Test
+    public void addStudentBVAInvalidEmail() {
         Student student = new Student("4", "Raul Mogos", 935, "");
         try {
             studentValidator.validate(student);
